40b687eee1c85079d06712e642ea681aedf4d8ca
Gaurav Tanpure <gauravtanpure999@gmail.com>
2025-10-04 19:54:17 +0530

    mobile search bar removed

--- vadak-bhashan-shala/src/components/Header.tsx
+++ vadak-bhashan-shala/src/components/Header.tsx
@@ -175,7 +175,7 @@ const Header: React.FC = () => {
         </div>
 
         {/* Mobile Search - kept for continuity */}
-        <div className="lg:hidden py-3 border-t border-border">
+        {/* <div className="lg:hidden py-3 border-t border-border">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
             <input
@@ -184,7 +184,7 @@ const Header: React.FC = () => {
               className="w-full pl-10 pr-4 py-2 border border-input rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent text-sm"
             />
           </div>
-        </div>
+        </div> */}
       </div>
 
       {/* Mobile Menu */}
